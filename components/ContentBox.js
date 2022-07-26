cf177d7b6ed83ced4b975b5ccfeb969268f3e5b4
Lee Stevens <leestevens@Office-Mac.local>
2022-07-26 13:36:13 +0100

    initial commit

---
+++ components/ContentBox.js
@@ -0,0 +1,4 @@
+import styles from "./ContentBox.module.css";
+export const ContentBox = ({ children }) => {
+return <div className={styles.wrapper}>{children}</div>;
+};
---
+++ components/Header.js
@@ -0,0 +1,4 @@
+import styles from "./Header.module.css";
+export const Header = ({ children }) => {
+return <div className={styles.wrapper}>{children}</div>;
+};
